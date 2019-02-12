c01926012f8d5357a6a6b9db9b37b4786c37a6b1
ESTBLC <estblcsk@gmail.com>
2019-02-12 17:18:19 +0000

    Add specific file for registers

--- src/regs/regs.h
+++ src/regs/regs.h
@@ -1,4 +1,9 @@
+#ifndef REGS_H
+#define REGS_H
+
 #include <sys/types.h>
 #include <sys/user.h>
 
-inline void regs_get(pid_t pid, struct user_regs_struct *regs);
+void regs_get(pid_t pid, struct user_regs_struct *regs);
+
+#endif
